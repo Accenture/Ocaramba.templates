b5d2a5d2a521c8f4a576df3b039e1aa6cd1bbba0
Jakub Raczek <jraczek@objectivity.co.uk>
2019-08-21 21:49:24 +0200

    Added Ocaramba.PageObjectClassTemplate

---
+++ Ocaramba.PageObjectClassTemplate/Class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using NLog;
+using Ocaramba.Extensions;
+using Ocaramba.Types;
+
+namespace $rootnamespace$
+{
+    public class $safeitemrootname$ : ProjectPageBase
+    {
+        private static readonly NLog.Logger Logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Locators for elements
+        /// </summary>
+        private readonly ElementLocator
+            linkLocator = new ElementLocator(Locator.CssSelector, "a[href='/{0}']");
+
+        public $safeitemrootname$(DriverContext driverContext)
+            : base(driverContext)
+        {
+        }
+
+        /// <summary>
+        /// Methods for this HomePage
+        /// </summary>
+        public void OpenHomePage()
+        {
+            var url = BaseConfiguration.GetUrlValue;
+            this.Driver.NavigateTo(new Uri(url));
+            Logger.Info(CultureInfo.CurrentCulture, "Opening page {0}", url);
+        }
+
+    }
+}
+
