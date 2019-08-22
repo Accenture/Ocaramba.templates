3a6a72e24e128faf3e4a57af422afb79771e3659
Jakub Raczek <jraczek@objectivity.co.uk>
2019-08-22 07:54:37 +0200

    Added Ocaramba.TestNunit.Class.Template

---
+++ Ocaramba.TestNunit.Class.Template/Class.cs
@@ -0,0 +1,19 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/ObjectivityLtd/Ocaramba 
+
+using NUnit.Framework;
+using Ocaramba;
+
+namespace $rootnamespace$
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class $safeitemrootname$ : ProjectTestBase
+    {
+        [Test]
+        public void Test()
+        {
+            // TODO: Add your test code here
+        }
+    }
+}
